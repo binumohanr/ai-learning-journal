126ef4a9667d298af705d0fe7fa1a582a55ad039
Binu Mohan <binumohanr@gmail.com>
2026-03-02 14:46:45 +1100

    Add first pythin script

---
+++ hello-it.py
@@ -0,0 +1,10 @@
+# My first Python script
+# Binu Mohan - IT engineer learning AI
+
+Name = "Binu"
+role = "AI Engineer"
+goal = "AI Builder"
+print("Hello from python!!")
+print("My name is " + Name)
+print("I am an " + role)
+print("My goal is to be an " + goal)
